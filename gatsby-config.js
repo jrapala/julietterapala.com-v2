32915f253de5d3214e857893ce5a1e944b6c3268
Juliette Rapala <juliette@eventric.com>
2019-09-09 20:40:22 -0500

    Debug gatsby-remark-images bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,6 +49,7 @@ module.exports = {
 		`gatsby-plugin-offline`,
 		`gatsby-plugin-react-helmet`,
 		`gatsby-plugin-styled-components`,
+		`gatsby-remark-images`,
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
