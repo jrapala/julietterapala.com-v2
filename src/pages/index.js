b700bb1f0c69c7c952d31f1c3453bc57467e1444
Juliette Rapala <juliette@eventric.com>
2019-09-10 08:59:36 -0500

    Fix fontawesome bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,12 @@ import Layout from '../components/Layout'
 import Seo from '../components/Seo'
 import Hero from '../components/Hero'
 
+// This ensures that the icon CSS is loaded immediately before attempting to render icons
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+// Prevent fontawesome from dynamically adding its css since we did it manually above
+config.autoAddCss = false
+
 const App = () => (
 	<Layout>
 		<Seo
